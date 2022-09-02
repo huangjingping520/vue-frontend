f94962e7c3f2f3ae997099d90f26bd0900cd9ab4
MerlinAlex <merlinalex@qq.com>
2022-09-02 14:51:56 +0800

    chore: init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+const title = ref('hello')
+</script>
+
+<template>
+  <view class="content">
+
+  </view>
+</template>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    uni(),
+    Components(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/, /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        'vue-router',
+        // custom
+        {
+          '@vueuse/core': [
+            // named imports
+            'useMouse', // import { useMouse } from '@vueuse/core',
+            // alias
+            ['useFetch', 'useMyFetch'], // import { useFetch as useMyFetch } from '@vueuse/core',
+          ],
+          'axios': [
+            // default imports
+            ['default', 'axios'], // import { default as axios } from 'axios',
+          ],
+          '[package-name]': [
+            '[import-names]',
+            // alias
+            ['[from]', '[alias]'],
+          ],
+        },
+      ]
+    })
+  ],
+})
